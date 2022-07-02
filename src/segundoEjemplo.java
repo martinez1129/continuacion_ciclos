9a2b001f91a6da45b9ee6f0a335577e55852f449
carenmartinez <flakitamart03@gmail.com>
2022-07-02 08:33:18 -0500

    se creo ejemplo 2

---
+++ src/segundoEjemplo.java
@@ -0,0 +1,11 @@
+public class segundoEjemplo {
+    public static void main (String [] args){
+        //escribir los numeros del 10 al 2
+        int numero = 10;
+        while (numero >= 1){
+            System.out.println("numero: "+numero);
+            //numero--;
+            numero = numero - 1;
+        }
+    }
+}
